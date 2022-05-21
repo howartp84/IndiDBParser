270a856f269dee77c6f4b5d0a2d8cd01ab2e3ccc
howartp84 <PHowarth@BlueYonder.co.uk>
2022-05-21 18:04:12 +0100

    v3.0.5

--- IndiDB Parser.indigoPlugin/Contents/Server Plugin/plugin.py
+++ IndiDB Parser.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -26,6 +26,7 @@ class Plugin(indigo.PluginBase):
 	def __init__(self, pluginId, pluginDisplayName, pluginVersion, pluginPrefs):
 		super(Plugin, self).__init__(pluginId, pluginDisplayName, pluginVersion, pluginPrefs)
 		self.debug = pluginPrefs.get("showDebugInfo", False)
+		self.debug = True
 
 	def startup(self):
 		self.parseDB(valuesDict=None)
@@ -48,12 +49,14 @@ class Plugin(indigo.PluginBase):
 		#indigo.server.log("getCWD: {}".format(os.getcwd()))
 		os.chdir("{}".format(liveDBPath)) #Might not be the getInstallFolderPath()!
 		#indigo.server.log("getCWD: {}".format(os.getcwd()))
+
+###############################################################################################
+
 		self.debugLog("Copying Indigo DB for safety")
 
-		indigo.server.log("cp {} {}".format(liveDBNameExt,tempDBNameExt))
-		os.system('cp {} {}'.format(liveDBNameExt,tempDBNameExt))
-
-		indigo.server.log("shutil.copy({},{})".format(liveDBNameExt,tempDBNameExt))
+		#indigo.server.log("cp {} {}".format(liveDBNameExt,tempDBNameExt))
+		#os.system('cp {} {}'.format(liveDBNameExt,tempDBNameExt))
+		#indigo.server.log("shutil.copy({},{})".format(liveDBNameExt,tempDBNameExt))
 		shutil.copy(liveDBNameExt,tempDBNameExt)
 
 		file_exists = os.path.exists(tempDBNameExt)
@@ -79,72 +82,94 @@ class Plugin(indigo.PluginBase):
 		out = {}
 		out['plugins'] = {}
 		out['actions'] = {}
+		out['devices'] = {}
+
+###############################################################################################
 
 		self.debugLog("Parsing Indigo Actions")
 		for a in db['ActionGroupList']['ActionGroup']:		#Loop through Indigo Actions in DB
-			lastPluginID = ""
-			lastPluginAction = ""
 			actionName = a['Name']['#text']
 			actionID = a['ID']['#text']
-			if (isinstance(a['ActionSteps']['Action'],dict)):
-				a['ActionSteps']['Action'] = [a['ActionSteps']['Action']]				#Force single Actions to be a list of 1 Action
-			for aStep in a['ActionSteps']['Action']:													#Loop through each Step in the Action
-				if 'PluginID' in aStep:
-					print("Action: {}".format(actionName))
-					if (isinstance(aStep,dict)):
-						pluginID = aStep['PluginID']['#text']
+			if ('Action' in a['ActionSteps']):
+				if (isinstance(a['ActionSteps']['Action'],dict)):
+					a['ActionSteps']['Action'] = [a['ActionSteps']['Action']]				#Force single Actions to be a list of 1 Action
+				for aStep in a['ActionSteps']['Action']:													#Loop through each Step in the Action
+					if 'PluginID' in aStep:
+						if (isinstance(aStep,dict)):
+							pluginID = aStep['PluginID']['#text']
+						else:
+							pluginID = str(aStep)
+
+						if 'TypeLabelPlugin' in aStep:
+							pluginAction = aStep['TypeLabelPlugin']['#text']
+						else:
+							pluginAction = "[Step has no name; probably builtin Indigo plugin]"
+
+						if 'DeviceID' in aStep:
+							deviceID = aStep['DeviceID']['#text']
+							pluginAction = "{}  [Device: {}]".format(pluginAction,localDevices[int(deviceID)].name)
+
+
+						#Build $Plugins
+						if pluginID not in out["plugins"]:
+							out["plugins"][pluginID] = {}
+							out["plugins"][pluginID]["actions"] = {}
+						if actionID not in out["plugins"][pluginID]["actions"]:
+							out["plugins"][pluginID]["actions"][actionID] = []
+						out["plugins"][pluginID]["actions"][actionID].append(pluginAction)
+
+						#Build $Actions
+						if actionID not in out["actions"]:
+							out["actions"][actionID] = {}
+						if pluginID not in out["actions"][actionID]:
+							out["actions"][actionID][pluginID] = []
+						out["actions"][actionID][pluginID].append(pluginAction)
+
+###############################################################################################
+
+		self.debugLog("Parsing Indigo Devices")
+		for d in db['DeviceList']['Device']:		#Loop through Indigo Devices in DB
+			deviceName = d['Name']['#text']
+			deviceID = d['ID']['#text']
+			if 'PluginID' in d:
+				if (isinstance(d,dict)):
+					pluginID = d['PluginID']['#text']
+				else:
+					pluginID = str(d)
+
+				if 'PluginUiName' in d:
+					if (isinstance(d,dict)):
+						pluginName = d['PluginUiName']['#text']
 					else:
-						pluginID = str(aStep)
-					if (pluginID != lastPluginID):
-						print("|---{}".format(pluginID))
-						lastPluginID = pluginID
+						pluginName = str(d)
 
-					if 'TypeLabelPlugin' in aStep:
-#						if (isinstance(aStep,dict)):
-#							pluginAction = aStep['TypeLabelPlugin']['#text']
-#							indigo.server.log("Here 76 {}".format(pluginAction))
-#						else:
-#							pluginAction = aStep
-#							indigo.server.log("Here 78 {}".format(pluginAction))
-#						if (pluginAction != lastPluginAction):
-#							print("|---|--{}".format(pluginAction))
-#							lastPluginAction = pluginAction
-						pluginAction = aStep['TypeLabelPlugin']['#text']
-					else:
-						pluginAction = "[Step has no name; probably builtin Indigo plugin]"
+				if 'TypeName' in d:
+					deviceType = d['TypeName']['#text']
+				else:
+					deviceType = "[Unidentified]"
 
-					if 'DeviceID' in aStep:
-#						if (isinstance(aStep,dict)):
-#							pluginAction = aStep['TypeLabelPlugin']['#text']
-#							indigo.server.log("Here 76 {}".format(pluginAction))
-#						else:
-#							pluginAction = aStep
-#							indigo.server.log("Here 78 {}".format(pluginAction))
-#						if (pluginAction != lastPluginAction):
-#							print("|---|--{}".format(pluginAction))
-#							lastPluginAction = pluginAction
-						deviceID = aStep['DeviceID']['#text']
-						pluginAction = "{}  [Device: {}]".format(pluginAction,localDevices[int(deviceID)].name)
+				deviceDesc = "{}  [Type: {}]".format(deviceName,localDevices[int(deviceID)].model)
+
+				#self.debugLog(out["plugins"])
+				#Build $Plugins
+				if pluginID not in out["plugins"]:
+					out["plugins"][pluginID] = {}
+				out["plugins"][pluginID]["devices"] = {}
+				if actionID not in out["plugins"][pluginID]["devices"]:
+					out["plugins"][pluginID]["devices"][deviceID] = []
+				out["plugins"][pluginID]["devices"][deviceID].append(deviceDesc)
+
+				#Build $Devices
+				if deviceID not in out["devices"]:
+					out["devices"][deviceID] = {}
+				if pluginID not in out["devices"][deviceID]:
+					out["devices"][deviceID][pluginID] = []
+				out["devices"][deviceID][pluginID].append(deviceDesc)
 
 
-					#Build $Plugins
-					if pluginID not in out["plugins"]:
-						out["plugins"][pluginID] = {}
-					if actionID not in out["plugins"][pluginID]:
-						out["plugins"][pluginID][actionID] = []
-					out["plugins"][pluginID][actionID].append(pluginAction)
-
-					#Build $Actions
-					if actionID not in out["actions"]:
-						out["actions"][actionID] = {}
-					if pluginID not in out["actions"][actionID]:
-						out["actions"][actionID][pluginID] = []
-					out["actions"][actionID][pluginID].append(pluginAction)
-
-			print("|")
 
 		doOut = True
-		#doOut = False
+		doOut = False
 		if (doOut):
 
 			self.debugLog("Building output for txt files")
@@ -154,14 +179,16 @@ class Plugin(indigo.PluginBase):
 			outPluginLines = []
 
 			for pID in out["plugins"]:
-				#self.debugLog("Plugin: {}".format(pID))
-				outPluginLines.append("Plugin: {}\n\r".format(pID))
-				for aID in out["plugins"][pID]:
-					#self.debugLog("|------{} [{}]".format(aID,indigo.actionGroups[int(aID)].name))
-					outPluginLines.append("|------{} [{}]\n\r".format(aID,indigo.actionGroups[int(aID)].name))
-					for step in out["plugins"][pID][aID]:
-						#self.debugLog("|------|------{}".format(step))
-						outPluginLines.append("|------|------{}\n\r".format(step))
+				outPluginLines.append("Plugin: {}\n".format(pID))
+				outPluginLines.append("|------Actions:\n")
+				if ("actions" in ["plugins"][pID]):
+					for aID in out["plugins"][pID]["actions"]:
+						outPluginLines.append("|------|------{} [{}]\n".format(indigo.actionGroups[int(aID)].name,aID))
+						for step in out["plugins"][pID]["actions"][aID]:
+							outPluginLines.append("|------|------|------{}\n".format(step))
+				if ("devices" in ["plugins"][pID]):
+					for dDesc in out["plugins"][pID]["devices"]:
+						outPluginLines.append("|------|------{}\n".format(dDesc))
 
 			with open('Plugins.txt','w') as f:
 				f.write(''.join(outPluginLines))
